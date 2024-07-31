bcc01bf8e00fa2ed3be85b26260f97e49e63a377
BrunoSola <brunosola3@gmail.com>
2024-07-31 04:35:53 -0300

    feat: Implementado os DTOS de Role e User. Adicionado UserDetails e GrantedAuthority em User e Role.

---
+++ src/main/java/com/devsuperior/demo/dto/UserDTO.java
@@ -0,0 +1,59 @@
+package com.devsuperior.demo.dto;
+
+import com.devsuperior.demo.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.*;
+
+public class UserDTO {
+
+    private Long id;
+    private String email;
+    private String password;
+
+    private List<String> roles = new ArrayList<>();
+
+    public UserDTO() {
+    }
+
+    public UserDTO(Long id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+    public UserDTO(User entity) {
+        id = entity.getId();
+        email = entity.getEmail();
+        for (GrantedAuthority role : entity.getAuthorities()){
+            roles.add(role.getAuthority());
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+}
